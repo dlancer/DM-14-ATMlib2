4e6278989ea4dad9cf77cefc121ffe365de26b43
Joeri Gantois <info@JO3RI.be>
2018-03-11 12:06:36 +0100

    replaced the SFX with a coin pick up effect

--- ATMlib2_AB/sfx.h
+++ ATMlib2_AB/sfx.h
@@ -3,15 +3,16 @@
 
 const PROGMEM struct sfx1_data {
   uint8_t fmt;
-  uint8_t pattern0[9];
+  uint8_t pattern0[12];
 } sfx1 = {
   .fmt = ATM_SCORE_FMT_MINIMAL_MONO,
   .pattern0 = {
-    ATM_CMD_M_SET_TEMPO(22),
+    ATM_CMD_M_SET_TEMPO(32),
     ATM_CMD_M_SET_VOLUME(127),
-    ATM_CMD_I_NOTE_F5,
-    ATM_CMD_M_DELAY_TICKS(11),
-    ATM_CMD_I_NOTE_OFF,
+    ATM_CMD_M_SLIDE_VOL_ON(-8),
+    ATM_CMD_I_NOTE_F6,
+    ATM_CMD_M_DELAY_TICKS(5),
+    ATM_CMD_I_NOTE_G6,
     ATM_CMD_M_DELAY_TICKS(11),
     ATM_CMD_I_STOP,
   },
