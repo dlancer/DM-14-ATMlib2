61b53fc49da0bef614dc8331285f6b2d93d8d502
Delio Brignoli <brignoli.delio@gmail.com>
2018-03-12 22:26:26 +0100

    Make some noise: add 3 SFX
    Thing flying past
    Machine gun
    Bouncing thing

--- ATMlib2_AB/inputs.h
+++ ATMlib2_AB/inputs.h
@@ -17,11 +17,20 @@ void checkInputs()
       case 2:
         atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx3, &sfx_state);
         break;
+      case 3:
+        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx4, &sfx_state);
+        break;
+      case 4:
+        atm_synth_play_sfx_track(OSC_CH_THREE, (const uint8_t*)&sfx5, &sfx_state);
+        break;
+      case 5:
+        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx6, &sfx_state);
+        break;
     }
   }
 
   if (arduboy.justPressed(A_BUTTON)) {
-    sfxState = (++sfxState) %3;
+    sfxState = (++sfxState) % 6;
   }
 
   if (arduboy.justPressed(LEFT_BUTTON)) {
--- ATMlib2_AB/sfx.h
+++ ATMlib2_AB/sfx.h
@@ -51,5 +51,62 @@ const PROGMEM struct sfx3_data { //12 +3
   },
 };
 
+const PROGMEM struct sfx4_data {
+  uint8_t fmt;
+  uint8_t pattern0[18];
+} sfx4 = {
+  .fmt = ATM_SCORE_FMT_MINIMAL_MONO,
+  .pattern0 = {
+    ATM_CMD_M_SET_TEMPO(48),
+    ATM_CMD_M_SET_VOLUME(0),
+    ATM_CMD_M_VIBRATO_ON(-10,24),
+    ATM_CMD_M_SLIDE_VOL_ON(8),
+    ATM_CMD_I_NOTE_F5,
+    ATM_CMD_M_DELAY_TICKS(24),
+    ATM_CMD_M_SLIDE_VOL_ON(-8),
+    ATM_CMD_M_DELAY_TICKS(24),
+    ATM_CMD_I_STOP,
+  },
+};
+
+const PROGMEM struct sfx5_data {
+  uint8_t fmt;
+  uint8_t pattern0[18];
+} sfx5 = {
+  .fmt = ATM_SCORE_FMT_MINIMAL_MONO,
+  .pattern0 = {
+    ATM_CMD_M_SET_TEMPO(48),
+    ATM_CMD_M_SET_VOLUME(63),
+    ATM_CMD_M_TREMOLO_ON(10,2),
+    ATM_CMD_I_NOTE_F5,
+    ATM_CMD_M_DELAY_TICKS(24),
+    ATM_CMD_M_DELAY_TICKS(24),
+    ATM_CMD_I_STOP,
+  },
+};
+
+const PROGMEM struct sfx6_data {
+  uint8_t fmt;
+  uint8_t pattern0[30];
+} sfx6 = {
+  .fmt = ATM_SCORE_FMT_MINIMAL_MONO,
+  .pattern0 = {
+    ATM_CMD_M_SET_TEMPO(48),
+    ATM_CMD_M_SET_VOLUME(127),
+    ATM_CMD_M_SLIDE_VOL_ON(-32),
+    ATM_CMD_I_NOTE_G6,
+    ATM_CMD_M_DELAY_TICKS(8),
+    ATM_CMD_M_SET_VOLUME(96),
+    ATM_CMD_M_DELAY_TICKS(16),
+    ATM_CMD_M_SET_VOLUME(64),
+    ATM_CMD_M_DELAY_TICKS(32),
+    ATM_CMD_M_SET_VOLUME(32),
+    ATM_CMD_M_DELAY_TICKS(32),
+    ATM_CMD_M_DELAY_TICKS(32),
+    ATM_CMD_M_SET_VOLUME(10),
+    ATM_CMD_M_DELAY_TICKS(10),
+    ATM_CMD_I_STOP,
+  },
+};
 
 #endif
