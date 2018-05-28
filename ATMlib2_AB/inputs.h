bcf96a9064e71f93d2c6e8b9907bb3492bb351aa
Delio Brignoli <brignoli.delio@gmail.com>
2018-05-28 12:38:16 +0200

    Update code to compile against changes to ATMlib2's API

--- ATMlib2_AB/inputs.h
+++ ATMlib2_AB/inputs.h
@@ -9,52 +9,52 @@ void checkInputs()
     switch (sfxState)
     {
       case 0:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx0, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx0);
         break;
       case 1:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx1, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx1);
         break;
       case 2:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx2, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx2);
         break;
       case 3:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx3, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx3);
         break;
       case 4:
-        atm_synth_play_sfx_track(OSC_CH_THREE, (const uint8_t*)&sfx4, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_3, 0, (const uint8_t*)&sfx4);
         break;
       case 5:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx5, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx5);
         break;
       case 6:
-        atm_synth_play_sfx_track(OSC_CH_THREE, (const uint8_t*)&sfx6, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_3, 0, (const uint8_t*)&sfx6);
         break;
       case 7:
-        atm_synth_play_sfx_track(OSC_CH_THREE, (const uint8_t*)&sfx7, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_3, 0, (const uint8_t*)&sfx7);
         break;
       case 8:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx8, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx8);
         break;
       case 9:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx9, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx9);
         break;
       case 10:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx10, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx10);
         break;
       case 11:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx11, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx11);
         break;
       case 12:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx12, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx12);
         break;
       case 13:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx13, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx13);
         break;
        case 14:
-        atm_synth_play_sfx_track(OSC_CH_THREE, (const uint8_t*)&sfx14, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_3, 0, (const uint8_t*)&sfx14);
         break;
        case 15:
-        atm_synth_play_sfx_track(OSC_CH_TWO, (const uint8_t*)&sfx15, &sfx_state);
+        atm_synth_play_sfx_track(OSC_CH_2, 0, (const uint8_t*)&sfx15);
         break;
     }
   }
@@ -64,11 +64,11 @@ void checkInputs()
   }
 
   if (arduboy.justPressed(LEFT_BUTTON)) {
-    atm_synth_play_score((const uint8_t*)&score);
+    atm_synth_start_score((const uint8_t*)&score);
   }
 
   if (arduboy.justPressed(RIGHT_BUTTON)) {
-    atm_synth_set_score_paused(!atm_synth_get_score_paused());
+    atm_synth_set_score_paused(atm_synth_is_score_playing());
   }
 }
 
